4921f428d21896694291fe995c2cfba5b009df59
mehulnagpurkar <mehulnagpurkar@MacBook-Pro.local>
2018-06-08 19:31:46 +1000

    pandas and cv2 import check

---
+++ project_1.py
@@ -0,0 +1,27 @@
+
+import pandas as pd
+import numpy as np
+from scipy import stats
+
+import cv2
+
+cv2.haarcascades
+
+
+
+def multiply(a, b):
+    c = a * b
+
+    return c
+
+def check():
+
+    if 10:
+        print ('What is happening')
+
+
+if __name__ == '__main__':
+    answer = multiply(2, 3)
+    check()
+
+    print(answer)
